794b53ad3100088ef34ad260cf2037cc03a99e5a
caleb <calebtelfe@gmail.com>
2026-02-21 20:16:28 -0500

    msg query ids. can view individual msg. + / page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,23 +6,36 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
+    id: 1
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date(),
+    id: 2
   }
 ];
 
+indexRouter.get("/", (req, res) => res.render("index"));
 
-indexRouter.get("/", (req, res) => res.render("index", { messages: messages }));
+indexRouter.get("/messages", (req, res) => {
+  const msgID = req.query.id;
+
+  if (msgID) {
+    const singleMessage = messages.filter(msg => msg.id == msgID);
+    return res.render("messages", {messages: singleMessage});
+  }
+
+  res.render("messages", { messages: messages });
+});
 
 indexRouter.get("/new", (req, res) => res.render("form"));
 
 indexRouter.post("/new", (req, res) => {
   const data = req.body;
-  messages.push({text: data.text, user: data.author, added: new Date()});
+  const msgID = messages.length + 1;
+  messages.push({text: data.text, user: data.author, added: new Date(), id: msgID});
   res.redirect("/");
 });
 
